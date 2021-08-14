ba599342894121e29ad0ace6a5a0305f339b6674
Aman Singh <amanksofficial@gmail.com>
2021-08-15 03:55:30 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import { GlobalStyles } from './global-styles';
+import { App } from './App';
+import { firebase } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
+
+ReactDOM.render(
+  <FirebaseContext.Provider value={{ firebase }}>
+    <GlobalStyles />
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
+);
